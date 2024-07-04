0820b980e39aa7b7a2aaae63d0a7622159d57fb8
unknown <aymenbm171998@gmail.com>
2024-07-04 12:39:34 -1100

    first

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+
+
+      <body>
+       <nav className="bg-black text-white flex justify-between text-xl content-baseline"> <h1 className="m-2">Navbar</h1>  <button className="rounded p-1 bg-white text-black m-2">Sign in </button></nav> 
+      <main>  {children}</main>
+        
+        </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import { url } from "inspector";
 import Link from "next/link";
 
+
+const imgUrl = [ "https://utfs.io/f/a7d5e7a1-2b95-4aaf-a62c-773eafd467be-21qso.jpg",
+  "https://utfs.io/f/54d823fb-9145-4533-88e9-f2e329435020-21qsp.jpg",
+  "https://utfs.io/f/bc9e52ed-d824-429d-bb15-7ebec3f0cf84-2dmy.jpg"
+ ] 
+ const images = imgUrl.map((url, index )=> ({
+  url,
+  id : index + 1
+ }))
 export default function HomePage() {
   return (
-  <h1>hello</h1>
+<main>
+
+  <div className="flex flex-wrap justify-center ">
+    {images.map((image)=> (
+      <div   key={image.id}>
+        <img className="divimagesstyle m-2"  src={image.url} alt="" />
+      </div>
+    ))}
+  </div>
+</main>
+
   );
 }
