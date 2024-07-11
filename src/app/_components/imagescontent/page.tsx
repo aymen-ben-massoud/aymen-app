213a9584ed404178fcffba344ba6d7615cf216b8
aymen-ben-massoud <86793043+aymen-ben-massoud@users.noreply.github.com>
2024-07-11 12:40:00 -1100

    delete button fixed

--- src/app/_components/imagescontent/page.tsx
+++ src/app/_components/imagescontent/page.tsx
@@ -1,14 +1,9 @@
-import {   getImages} from "~/server/db/queries/queries";
-
-
-
+import {   DeleteImage, getImages} from "~/server/db/queries/queries";
 
 export const dynamic = "force-dynamic"
 
 export default async  function ImageContent() {
   const images = await getImages()
- 
-  
  
   return (
 <main>
@@ -20,6 +15,11 @@ export default async  function ImageContent() {
       <div   key={image.id }>
         <img className="divimagesstyle m-2"  src={image.url} alt="" />
         <p> {image.name} </p>
+        <form action={async ()=> {
+          "use server";
+          await DeleteImage(image.id);
+        } } ><button>delete</button></form>
+       
       </div>
     ))}
   </div>
--- src/server/db/queries/queries.ts
+++ src/server/db/queries/queries.ts
@@ -1,12 +1,15 @@
 
-"use server"
-
+import "server-only";
 
 import { db } from "~/server/db";
+import { images } from '../schema';
+import { eq } from 'drizzle-orm';
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/dist/server/api-utils";
+
 
 
 export async function getImages(){
-
     const images = await db.query.images.findMany(
         { orderBy:(model , {desc}) => desc(model.id)}
        )
@@ -14,7 +17,10 @@ export async function getImages(){
    images
     )
 }
-
+export async function DeleteImage(id:number){ 
+    await db.delete(images).where(eq(images.id, id ))
+    revalidatePath("/")
+}
 
 
 
